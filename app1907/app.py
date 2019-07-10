dad429e603f09721699bc5581e053858619b1884
dteer <dteer@qq.com>
2019-07-10 20:28:50 +0800

    t

--- app1907/app.py
+++ app1907/app.py
@@ -1,7 +1,12 @@
-from flask import Flask,render_template,request,redirect,session
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
+app.config.from_object("settings.Dev")
 
-print(app.config)
-# app.config['DEBUG'] = True
-app.config.from_object("setting.Pro")
+
+#1、endpoint为地址的名称，不写，默认为函数名
+#2、传参 int: <int:nid> str: <nid>
+@app.route('/index/<int:nid>',methods=['GET','POST'],endpoint='n1')
+def index(nid):
+    print(url_for('n1'))                    #url_for获取名称n1的地址
+    return 'Index'
